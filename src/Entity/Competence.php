9323e3d5b399550b551086e241c1b0a7ee440bd6
Vincent Sureau <vincentsureau5@gmail.com>
2019-01-28 15:35:08 +0100

    added competence datatable in specialisation show

--- src/Entity/Competence.php
+++ src/Entity/Competence.php
@@ -5,9 +5,12 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation AS Serializer;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CompetenceRepository")
+ * 
+ * @Serializer\ExclusionPolicy("all")
  */
 class Competence
 {
@@ -15,16 +18,22 @@ class Competence
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * 
+     * @Serializer\Expose()
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * 
+     * @Serializer\Expose()
      */
     private $reference;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * 
+     * @Serializer\Expose()
      */
     private $name;
 
