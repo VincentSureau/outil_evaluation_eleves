fbc39ecb85066369bb885486c51dfc38cf85e66f
Vincent Sureau <vincentsureau5@gmail.com>
2019-01-31 11:39:52 +0100

    added basic sticky footer + registration form style

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,24 +3,24 @@
 namespace App\Form;
 
 use App\Entity\User;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('grade')
+            ->add('lastname', TextType::class, ['label' => 'Indiquez votre nom:'])
+            ->add('firstname', TextType::class, ['label' => 'Indiquez votre prénom:'])
+            ->add('grade', TextType::class, ['label' => 'Indiquez votre grade:'])
+            ->add('S\'identifier', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-primary btn-lg']
+            ])
         ;
     }
 
