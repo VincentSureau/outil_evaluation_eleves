901d14239659bfb7a9b66fd38283d49923a45d92
zougui <zouguidu91@gmail.com>
2019-03-13 10:43:14 +0100

    templates/controllers backoffice done

---
+++ src/Controller/Back/CirfaController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller\Back;
+
+use App\Entity\Cirfa;
+use App\Form\Cirfa1Type;
+use App\Repository\CirfaRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/cirfa")
+ */
+class CirfaController extends AbstractController
+{
+    /**
+     * @Route("/", name="cirfa_index", methods={"GET"})
+     */
+    public function index(CirfaRepository $cirfaRepository): Response
+    {
+        return $this->render('back/cirfa/index.html.twig', [
+            'cirfas' => $cirfaRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="cirfa_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $cirfa = new Cirfa();
+        $form = $this->createForm(Cirfa1Type::class, $cirfa);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($cirfa);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('cirfa_index');
+        }
+
+        return $this->render('back/cirfa/new.html.twig', [
+            'cirfa' => $cirfa,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="cirfa_show", methods={"GET"})
+     */
+    public function show(Cirfa $cirfa): Response
+    {
+        return $this->render('back/cirfa/show.html.twig', [
+            'cirfa' => $cirfa,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="cirfa_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Cirfa $cirfa): Response
+    {
+        $form = $this->createForm(Cirfa1Type::class, $cirfa);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('cirfa_index', [
+                'id' => $cirfa->getId(),
+            ]);
+        }
+
+        return $this->render('back/cirfa/edit.html.twig', [
+            'cirfa' => $cirfa,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="cirfa_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Cirfa $cirfa): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$cirfa->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($cirfa);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('cirfa_index');
+    }
+}
---
+++ src/Controller/Back/SpecialisationController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller\Back;
+
+use App\Entity\Specialisation;
+use App\Form\Specialisation1Type;
+use App\Repository\SpecialisationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/specialisation")
+ */
+class SpecialisationController extends AbstractController
+{
+    /**
+     * @Route("/", name="specialisation_index", methods={"GET"})
+     */
+    public function index(SpecialisationRepository $specialisationRepository): Response
+    {
+        return $this->render('back/specialisation/index.html.twig', [
+            'specialisations' => $specialisationRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="specialisation_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $specialisation = new Specialisation();
+        $form = $this->createForm(Specialisation1Type::class, $specialisation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($specialisation);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('specialisation_index');
+        }
+
+        return $this->render('back/specialisation/new.html.twig', [
+            'specialisation' => $specialisation,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="specialisation_show", methods={"GET"})
+     */
+    public function show(Specialisation $specialisation): Response
+    {
+        return $this->render('back/specialisation/show.html.twig', [
+            'specialisation' => $specialisation,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="specialisation_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Specialisation $specialisation): Response
+    {
+        $form = $this->createForm(Specialisation1Type::class, $specialisation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('specialisation_index', [
+                'id' => $specialisation->getId(),
+            ]);
+        }
+
+        return $this->render('back/specialisation/edit.html.twig', [
+            'specialisation' => $specialisation,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="specialisation_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Specialisation $specialisation): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$specialisation->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($specialisation);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('specialisation_index');
+    }
+}
