024b3f932aa2287ebeca100eb068178f232daa4a
Vincent Sureau <vincentsureau5@gmail.com>
2019-03-14 14:57:11 +0100

    fixed datatable overflow

--- public/js/studentDatatable.js
+++ public/js/studentDatatable.js
@@ -1,6 +1,8 @@
 
     $(document).ready( function () {
         var table = $('#students_list').DataTable({
+            // responsive: true,
+            // scrollX: true,
             ajax: {
                 url: listUrl,
                 dataSrc: ''
@@ -23,17 +25,17 @@
             ],
         });
 
-        $('#filter td').each( function (i) {
-            var title = $('#students_list thead th').eq( $(this).index() ).text();
-            $(this).html( '<input type="text" placeholder="&#x1F50E; '+title+'" data-index="'+i+'" /><i class="fa fa-search" aria-hidden="true"></i>' );
-        } );
+        // $('#filter td').each( function (i) {
+        //     var title = $('#students_list thead th').eq( $(this).index() ).text();
+        //     $(this).html( '<input type="text" placeholder="&#x1F50E; '+title+'" data-index="'+i+'" /><i class="fa fa-search" aria-hidden="true"></i>' );
+        // } );
 
 
-        $( table.table().container() ).on( 'keyup', '#filter input', function () {
-            table
-                .column( $(this).data('index') )
-                .search( this.value )
-                .draw();
-        } );
+        // $( table.table().container() ).on( 'keyup', '#filter input', function () {
+        //     table
+        //         .column( $(this).data('index') )
+        //         .search( this.value )
+        //         .draw();
+        // } );
 
     } );
