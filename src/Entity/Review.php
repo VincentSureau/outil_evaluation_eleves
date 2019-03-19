63e360bf3d6ce1b4d783b451e35f347634f55902
Vincent Sureau <vincentsureau5@gmail.com>
2019-03-19 08:49:49 +0100

    fixed missingannotation in entities

--- src/Entity/Review.php
+++ src/Entity/Review.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation AS Serializer;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ReviewRepository")
